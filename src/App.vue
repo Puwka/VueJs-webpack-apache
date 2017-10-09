2243a4bb14bee0fcdfde8db94e88cf5e9bba3012
Puwka <gorokhov.inc@gmail.com>
2017-10-09 23:19:39 +0300

    some changes

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
       </v-list-tile>
     </v-list>
   </v-navigation-drawer>
-  <v-toolbar dark class="cyan darken-1">
+  <v-toolbar dark class="primary">
     <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
     <v-toolbar-title>
       <router-link to="/" tag="span" style="cursor: pointer">CoolApp</router-link>
@@ -25,100 +25,44 @@
   </v-toolbar>
   <main>
     <router-view>, link: ''</router-view>
-    <input type="text" v-model="name">
-    <select v-model="server">
-			<option value="euw1">EUW</option>
-			<option value="na1">NA</option>
-			<option value="ru">RU</option>
-		</select>
-    <p>Server: {{ server }}</p>
-    <p>Name: {{ name }}</p>
-    <p>League: {{ tier }}</p>
-    <p>Division: {{ rank }}</p>
-    <p>League Points: {{ lp }}</p>
-    <p>ID: {{ summonerId }}</p>
 
-    <button @click="getLeaguePos()">LeaguePos</button>
   </main>
 </v-app>
 </template>
 
 <script>
-var axios = require('axios')
 export default {
-  data () {
+  data() {
     return {
       sideNav: false,
       name: '',
-      summonerId: '',
-      lp: '',
-      rank: '',
-      tier: '',
       server: '',
       menuItems: [{
-        icon: 'supervisor_account',
-        title: 'Cool list',
-        link: '/meetups'
-      },
-      {
-        icon: 'room',
-        title: 'Stay cool',
-        link: '/meetup/new'
-      },
-      {
-        icon: 'person',
-        title: 'Profile',
-        link: '/profile'
-      },
-      {
-        icon: 'face',
-        title: 'Sign up',
-        link: '/signup'
-      },
-      {
-        icon: 'lock_open',
-        title: 'Sign in',
-        link: '/signin'
-      }
+          icon: 'supervisor_account',
+          title: 'Cool list',
+          link: '/meetups'
+        },
+        {
+          icon: 'room',
+          title: 'Stay cool',
+          link: '/meetup/new'
+        },
+        {
+          icon: 'person',
+          title: 'Profile',
+          link: '/profile'
+        },
+        {
+          icon: 'face',
+          title: 'Sign up',
+          link: '/signup'
+        },
+        {
+          icon: 'lock_open',
+          title: 'Sign in',
+          link: '/signin'
+        }
       ]
-    }
-  },
-  methods: {
-    doSmth: function () {
-      console.log('hi')
-      axios.post('../php/testing.php', {
-        name: this.name,
-        server: this.server
-      })
-        .then(response => {
-          console.log(response.data.name)
-          this.summonerId = response.data.id
-        })
-        .catch(e => {
-          console.log(e)
-        })
-    },
-    getLeaguePos: function () {
-      this.doSmth()
-      var self = this
-      this.lp = 'Loading...'
-      this.rank = 'Loading...'
-      this.tier = 'Loading...'
-      setTimeout(function () {
-        axios.post('../php/leaguePos.php', {
-          id: self.summonerId,
-          server: self.server
-        })
-          .then(response => {
-            console.log(response.data)
-            self.lp = response.data[0].leaguePoints
-            self.rank = response.data[0].rank
-            self.tier = response.data[0].tier
-          })
-          .catch(e => {
-            console.log(e)
-          })
-      }, 500)
     }
   }
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,97 @@
 <template>
-	<div>
-		<p>The Home Page</p>
-	</div>
+<v-container>
+  <v-layout row wrap>
+		<v-spacer></v-spacer>
+    <v-flex xs6 sm8>
+      <v-form>
+        <v-text-field label="Summoner Name" v-model="name" required></v-text-field>
+				{{ name }}
+      </v-form>
+    </v-flex>
+    <v-flex xs3 sm2 lg1>
+      <v-select v-bind:items="items" v-model="server" label="Server" single-line bottom></v-select>
+    </v-flex>
+		<v-flex xs3 sm2 lg1>
+			<v-btn raised large class="primary" dark @click.native="getLeaguePos()">GO!</v-btn>
+		</v-flex>
+		<v-spacer></v-spacer>
+  </v-layout>
+</v-container>
 </template>
+<script>
+var axios = require('axios')
+export default {
+  data() {
+    return {
+      name: null,
+      server: null,
+      summonerId: '',
+      lp: '',
+      rank: '',
+      tier: '',
+      items: [{
+          text: 'EUW',
+          value: 'euw1'
+        },
+        {
+          text: 'NA',
+          value: 'na1'
+        }
+      ]
+    }
+	},
+    methods: {
+      doSmth: function() {
+        console.log('hi')
+        axios.post('../php/testing.php', {
+            name: this.name,
+            server: this.server
+          })
+          .then(response => {
+            console.log(response.data.name)
+            this.summonerId = response.data.id
+          })
+          .catch(e => {
+            console.log(e)
+          })
+      },
+      getLeaguePos: function() {
+        this.doSmth()
+        var self = this
+        this.lp = 'Loading...'
+        this.rank = 'Loading...'
+        this.tier = 'Loading...'
+        setTimeout(function() {
+          axios.post('../php/leaguePos.php', {
+              id: self.summonerId,
+              server: self.server
+            })
+            .then(response => {
+              console.log(response.data)
+              self.lp = response.data[0].leaguePoints
+              self.rank = response.data[0].rank
+              self.tier = response.data[0].tier
+            })
+            .catch(e => {
+              console.log(e)
+            })
+        }, 500)
+      }
+    }
+  }
+
+</script>
+<!-- <input type="text" v-model="name">
+<select v-model="server">
+	<option value="euw1">EUW</option>
+	<option value="na1">NA</option>
+	<option value="ru">RU</option>
+</select>
+<p>Server: {{ server }}</p>
+<p>Name: {{ name }}</p>
+<p>League: {{ tier }}</p>
+<p>Division: {{ rank }}</p>
+<p>League Points: {{ lp }}</p>
+<p>ID: {{ summonerId }}</p>
+
+<button @click="getLeaguePos()">LeaguePos</button> -->
