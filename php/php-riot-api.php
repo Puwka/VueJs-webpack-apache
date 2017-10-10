e97619f5f8e2a2f39d952ff4be69b0a4003882eb
Puwka <gorokhov.inc@gmail.com>
2017-10-10 11:45:19 +0300

    Key change

--- php/php-riot-api.php
+++ php/php-riot-api.php
@@ -41,7 +41,7 @@ class riotapi {
 	const API_URL_SUMMONER_3 = 'https://{platform}.api.riotgames.com/lol/summoner/v3/';
 
 
-	const API_KEY = 'RGAPI-e2c537f8-5fed-4f4c-9a16-6988d03d5a7b';
+	const API_KEY = 'RGAPI-3177cd1d-8342-4099-bd7f-49b7d65939f4';
 
 	// Rate limit for 10 minutes
 	const LONG_LIMIT_INTERVAL = 600;
